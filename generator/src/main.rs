6641e2abb63b260410d8a7656662da0af5ab2277
Wren Turkal <wt@penguintechs.org>
2026-02-13 23:29:48 -0800

    Fix overriding logging config with env var.
    Due to a swapping of lines, the environment variable RUST_LOG was not able to
    override the level of logging to output. This fixes that.

--- generator/src/main.rs
+++ generator/src/main.rs
@@ -58,8 +58,8 @@ const GENERATE: &[Feature] = &[
 fn main() -> anyhow::Result<()> {
     tracing_subscriber::fmt()
         .with_writer(std::io::stderr)
-        .with_env_filter(EnvFilter::from_default_env())
         .with_max_level(LevelFilter::INFO)
+        .with_env_filter(EnvFilter::from_default_env())
         .init();
 
     verify_chiptool().context("chiptool is not installed")?;
