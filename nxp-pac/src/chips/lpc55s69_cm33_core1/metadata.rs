241dc8b50441e6d27eddc1dd39e8beda64c33495
Oleksandr <kozoriz407@gmail.com>
2026-09-15 13:37:33 +0300

    Return skipped pins

--- nxp-pac/src/chips/lpc55s69_cm33_core1/metadata.rs
+++ nxp-pac/src/chips/lpc55s69_cm33_core1/metadata.rs
@@ -2034,6 +2034,11 @@ pub const PERIPHERALS: &[Peripheral] = &[
                         iomuxc_daisy: None,
                     },
                     SignalPin {
+                        pin: "PIO0_4",
+                        alt: 8u8,
+                        iomuxc_daisy: None,
+                    },
+                    SignalPin {
                         pin: "PIO0_20",
                         alt: 1u8,
                         iomuxc_daisy: None,
@@ -2052,6 +2057,11 @@ pub const PERIPHERALS: &[Peripheral] = &[
                     SignalPin {
                         pin: "PIO0_2",
                         alt: 1u8,
+                        iomuxc_daisy: None,
+                    },
+                    SignalPin {
+                        pin: "PIO0_5",
+                        alt: 8u8,
                         iomuxc_daisy: None,
                     },
                     SignalPin {
@@ -2119,6 +2129,11 @@ pub const PERIPHERALS: &[Peripheral] = &[
                     SignalPin {
                         pin: "PIO0_19",
                         alt: 1u8,
+                        iomuxc_daisy: None,
+                    },
+                    SignalPin {
+                        pin: "PIO0_20",
+                        alt: 11u8,
                         iomuxc_daisy: None,
                     },
                     SignalPin {
